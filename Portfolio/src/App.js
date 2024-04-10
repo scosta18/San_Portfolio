665094ab58ba715a11f4c937e7d87b4e7ae38245
Sandro Costa <blaisecosta18@gmail.com>
2024-04-10 13:16:24 -0400

    hey

---
+++ Portfolio/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './Root/root'; // Import Header component
+
+function App() {
+  return (
+    <div className='appbk'>
+      <Router>
+      <div>
+        <Header />
+      </div>
+    </Router>
+    </div>
+  );
+}
+
+export default App;
